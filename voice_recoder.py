05b3da524c17f3bf73691d72b7a4ddc3c308cf43
Madusanka-Bandara <kasunmadhusanka44@gmail.com>
2024-10-13 21:21:42 +0530

    backup code before changes

--- voice_recoder.py
+++ voice_recoder.py
@@ -1,3 +1,4 @@
+# simple voice recoder using python
 import sounddevice as sd
 import wavio
 import argparse
@@ -5,7 +6,7 @@ import os
 import time
 
 class VoiceRecoder:
-    def __int__(self, filename = "recoding.wav", duration = 5, sample_rate = 44100, play_after = False):
+    def __init__(self, filename = "recoding.wav", duration = 5, sample_rate = 44100, play_after = False):
         """Initialize the voice recoder with default setting """
         self.filename = filename
         self.duration = duration
@@ -48,4 +49,33 @@ class VoiceRecoder:
             if self.play_after:
                 self.playing_audio(audio_data)
 
+def main():
+    """Main function for parse command-line arguments and start the voice recording """
+    parser = argparse.ArgumentParser(description="Simple Python Voice Recorder")
+    parser.add_argument("-f", "--file", file_type=str, default = "recording.wav", help="Output file name (default: recording.wav)")
+    parser.add_argument("-d", "--duration", type=int, default=5, help="Duration of recording in seconds (default: 5")
+    parser.add_argument("-r", "--rate", type=int, default=44100, help="Sample rate (default: 44100 Hz )")
+    parser.add_argument("-p","--play", action="store_true", help="play the audio after recording (optional) ")
 
+    args = parser.parse_args()
+
+    # check the file name has correct extension
+    if not args.file.endwith(".wav"):
+        print("Error: The file output must be a .wav file")
+        return
+
+    # check if output file exists and confirm overwrite
+    if os.path.exists(args.file):
+        confirm = input(f"File '{args.file}' already exists. Overwrite ? (y/n): ")
+        if confirm.lower() != 'y':
+            print("Recording Canceled ")
+            return
+
+    # initialize the voice recoder with user argument
+    recoder = VoiceRecoder(filename=args.file, duration = args.duration, sample_rate = args.sample_rate, play_after = args.play_after )
+
+    # Start recording
+    recoder.start_recording()
+
+if __name__ == "__main__":
+    main()
