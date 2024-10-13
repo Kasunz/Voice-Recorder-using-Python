ed7048f1e8e44b17228b23bb81e7cdd195d013cd
Madusanka-Bandara <kasunmadhusanka44@gmail.com>
2024-10-13 21:34:35 +0530

    Fixed issues with class initialization and argument handling in voice recorder.

--- voice_recoder.py
+++ voice_recoder.py
@@ -3,9 +3,8 @@ import sounddevice as sd
 import wavio
 import argparse
 import os
-import time
 
-class VoiceRecoder:
+class VoiceRecorder:
     def __init__(self, filename = "recoding.wav", duration = 5, sample_rate = 44100, play_after = False):
         """Initialize the voice recoder with default setting """
         self.filename = filename
@@ -19,6 +18,7 @@ class VoiceRecoder:
         try:
             audio_data = sd.rec(int(self.duration * self.sample_rate), samplerate = self.sample_rate, channels= 2)
             sd.wait() # wait until recoding is finished
+            return audio_data
 
         except Exception as e:
             print(f"Error recoding audio: {e}")
@@ -28,7 +28,7 @@ class VoiceRecoder:
         """Save the recorded audio to a wav file """
         try:
             wavio.write(self.filename, audio_data, self.sample_rate, sampwidth=2 )
-            print(f"Recording save to {self.filename}")
+            print(f"Recording saved to {self.filename}")
         except Exception as e:
             print(f"Error saving audio: {e}")
 
@@ -52,7 +52,7 @@ class VoiceRecoder:
 def main():
     """Main function for parse command-line arguments and start the voice recording """
     parser = argparse.ArgumentParser(description="Simple Python Voice Recorder")
-    parser.add_argument("-f", "--file", file_type=str, default = "recording.wav", help="Output file name (default: recording.wav)")
+    parser.add_argument("-f", "--file", type=str, default = "recording.wav", help="Output file name (default: recording.wav)")
     parser.add_argument("-d", "--duration", type=int, default=5, help="Duration of recording in seconds (default: 5")
     parser.add_argument("-r", "--rate", type=int, default=44100, help="Sample rate (default: 44100 Hz )")
     parser.add_argument("-p","--play", action="store_true", help="play the audio after recording (optional) ")
@@ -60,7 +60,7 @@ def main():
     args = parser.parse_args()
 
     # check the file name has correct extension
-    if not args.file.endwith(".wav"):
+    if not args.file.endswith(".wav"):
         print("Error: The file output must be a .wav file")
         return
 
@@ -72,10 +72,10 @@ def main():
             return
 
     # initialize the voice recoder with user argument
-    recoder = VoiceRecoder(filename=args.file, duration = args.duration, sample_rate = args.sample_rate, play_after = args.play_after )
+    recorder = VoiceRecorder(filename=args.file, duration=args.duration, sample_rate=args.rate, play_after=args.play)
 
     # Start recording
-    recoder.start_recording()
+    recorder.start_recording()
 
 if __name__ == "__main__":
     main()
